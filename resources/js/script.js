47a0060a8396f3fcb6b6476f066cb17fbbdd49ce
Ana Paula Rye Ito <a.p.ryeito@gmail.com>
2025-06-28 20:21:26 -0300

    caminho do script corrigido e barra de pesquisa implementada

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,47 +1,47 @@
 var userList = []
 var count = 1;
 
-function addUser(name,email){
-    var newUser = {id: count++,date: new Date().toISOString().slice(0, 10) ,name: name, email: email};
+function addUser(name, email) {
+    var newUser = { id: count++, date: new Date().toISOString().slice(0, 10), name: name, email: email };
     userList.push(newUser);
     localStorage.setItem('users', JSON.stringify(userList));
     renderUserList();
 }
 
-function deleteUser(userId){
-    var updatedUserList = userList.filter(function(user){
+function deleteUser(userId) {
+    var updatedUserList = userList.filter(function (user) {
         return user.id !== userId;
     });
 
-    if(updatedUserList.length < userList.length){
+    if (updatedUserList.length < userList.length) {
         userList = updatedUserList;
         localStorage.setItem('users', JSON.stringify(userList));
         renderUserList();
-    }else{
+    } else {
         alert('Usuário não encontrado');
     }
 }
 
-function getUserList(){
+function getUserList() {
     var storedUsers = JSON.parse(localStorage.getItem('users'));
     userList = storedUsers || [];
 }
 
-function renderUserList(){
+function renderUserList() {
     var userListElement = document.getElementById('userList');
     userListElement.innerHTML = '';
 
-    userList.forEach(function(user){
+    userList.forEach(function (user) {
         var userItem = document.createElement('li');
-        userItem.innerHTML = 'Data: <span class="user-date">' + user.date + '| Nome: </span> <span class="user-name">' + user.name + '|  Email:</span>'+'</span> <span class="user-email">' + user.email + '</span>' + '<button class="delete-button" onclick="deleteUser(' + user.id + ')">Excluir</button>';
+        userItem.innerHTML = 'Data: <span class="user-date">' + user.date + '| Nome: </span> <span class="user-name">' + user.name + '|  Email:</span>' + '</span> <span class="user-email">' + user.email + '</span>' + '<button class="delete-button" onclick="deleteUser(' + user.id + ')">Excluir</button>';
         userListElement.appendChild(userItem);
-        });
+    });
 }
 
 getUserList();
 renderUserList();
 
-document.getElementById('userForm').addEventListener('submit', function(event){
+document.getElementById('userForm').addEventListener('submit', function (event) {
     event.preventDefault();
     var nameInput = document.getElementById('nameInput');
     var emailInput = document.getElementById('emailInput');
@@ -51,15 +51,34 @@ document.getElementById('userForm').addEventListener('submit', function(event){
     emailInput.value = '';
 });
 
-function clearFields(){
+function clearFields() {
     const name = document.querySelector('#nameInput');
     const email = document.querySelector('#emailInput');
     name.value = '';
     email.value = '';
 };
 
-function clearList(){
+function clearList() {
     localStorage.clear();
     userList = [];
     renderUserList();
+}
+
+function searchByName() {
+    var input, filter, ul
+
+    input = document.getElementById("searchInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("userList");
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        // a = li[i].getElementsByTagName("a")[0];
+        txtValue = li[i].textContent || li[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
